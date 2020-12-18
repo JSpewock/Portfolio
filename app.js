3a04bf3aaf5d924863a44f8ae1b8630b1fe9c623
Jeremy <jeremy.spewock@gmail.com>
2020-12-17 20:11:26 -0500

    finished with adding projects just adding images now

--- app.js
+++ app.js
@@ -20,8 +20,11 @@ $(() => {
   $('<p>').text('HTML | JavaScript | CSS | Python | jQuery | NodeJS | Git |  GitHub | MongoDB | React.js | Postman | Flexbox | Bootstrap | Express.js | REST API | SQL |').attr('class', 'tech-skills').appendTo('#about')
   $('<div>').attr('id', 'projects').appendTo('#main')
   $('<h2>').text('Projects').attr('class', 'section').appendTo('#projects')
-  $('<div>').attr('id', 'project1').attr('class', 'project').appendTo('#projects')
   // I used https://stackoverflow.com/questions/2941189/how-to-overlay-one-div-over-another-div and https://api.jquery.com/hover/ to figure this hover thing out
+  // ----------------------------------
+  //             Project1
+  // ----------------------------------
+  $('<div>').attr('id', 'project1').attr('class', 'project').appendTo('#projects')
   $('<div>').attr('id', 'project1-img').attr('class', 'project-img').appendTo('#project1').hover(() => {
     $('#project1-info').css('display', 'block')
   },
@@ -36,7 +39,11 @@ $(() => {
   })
   $('<h1>').text('The Basic Pokemon Card Battle').attr('class', 'project-title').appendTo('#project1-info')
   $('<p>').text('This project was essentially a browser game that simulates a basic version of a pokemon card battle but you get random cards everytime! It was built using HTML, CSS, JavaScript, and jQuery as well as a third party api to collect the card data for the battle.').appendTo('#project1-info')
+  $('<img>').attr('src', './images/better-fit.jpg').attr('class', 'project-picture').appendTo('#project1-img')
 
+  // ----------------------------------
+  //               Project2
+  // ----------------------------------
   $('<div>').attr('id', 'project2').attr('class', 'project').appendTo('#projects')
   $('<div>').attr('id', 'project2-img').attr('class', 'project-img').appendTo('#project2').hover(() => {
     $('#project2-info').css('display', 'block')
@@ -53,6 +60,9 @@ $(() => {
   $('<h1>').text('GA Course Catalog').attr('class', 'project-title').appendTo('#project2-info')
   $('<p>').text('For this project I built a course catalog/study guide for General Assembly students in my class! This is a full CRUD app that was built using Node.js, Express, Mongoose, EJS, and Bcrypt.').appendTo('#project2-info')
 
+  // ---------------------------------
+  //             Project3
+  // ---------------------------------
   $('<div>').attr('id', 'project3').attr('class', 'project').appendTo('#projects')
   $('<div>').attr('id', 'project3-img').attr('class', 'project-img').appendTo('#project3').hover(() => {
     $('#project3-info').css('display', 'block')
@@ -68,6 +78,25 @@ $(() => {
   })
   $('<h1>').text('CockTails App').attr('class', 'project-title').appendTo('#project3-info')
   $('<p>').text('This project was actually a collaborative project that I worked on with 2 classmates. It is a MERN stack app that implements Mongo, Express, React, and Node to create a database for storing and searching for various cocktails. It also implements a third party api to increase the number of cocktails in the database.').appendTo('#project3-info')
+
+  // ------------------------------
+  //             Project4
+  // ------------------------------
+  $('<div>').attr('id', 'project4').attr('class', 'project').appendTo('#projects')
+  $('<div>').attr('id', 'project4-img').attr('class', 'project-img').appendTo('#project4').hover(() => {
+    $('#project4-info').css('display', 'block')
+  },
+  () => {
+    $('#project4-info').css('display', 'none')
+  }
+  )
+  $('<div>').attr('id', 'project4-info').attr('class', 'project-info').appendTo('#project4').hover(() => {
+    $('#project4-info').css('display', 'block')
+  }, () => {
+    $('#project4-info').css('display', 'none')
+  })
+  $('<h1>').text('Rantz').attr('class', 'project-title').appendTo('#project4-info')
+  $('<p>').text('This project is a discussion board based around ranting. This app was built using Python, Flask, and React and it features things such as jwts for user authentication.').appendTo('#project4-info')
   $('<div>').attr('id', 'contact-me').appendTo('#main')
   $('<h2>').text('Contact Me').attr('class', 'section').appendTo('#contact-me')
 })
